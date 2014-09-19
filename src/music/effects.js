d7f4e9d2f0a3cf1b6f955b63588dccab7b3435c6
Dario Seminara <darios3@gmail.com>
2014-09-19 12:10:45 -0300

    separated effects.js, implemented more generic formula effect

---
+++ src/music/effects.js
@@ -0,0 +1,39 @@
+MUSIC.Effects = MUSIC.Effects || {};
+
+MUSIC.Effects.Formula = function(audio, next, fcn) {
+  var scriptNode = audio.createScriptProcessor(1024, 1, 1);
+
+  scriptNode.onaudioprocess = function(audioProcessingEvent) {
+    // The input buffer is the song we loaded earlier
+    var inputBuffer = audioProcessingEvent.inputBuffer;
+
+    // The output buffer contains the samples that will be modified and played
+    var outputBuffer = audioProcessingEvent.outputBuffer;
+
+    // Loop through the output channels (in this case there is only one)
+    for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+      var inputData = inputBuffer.getChannelData(channel);
+      var outputData = outputBuffer.getChannelData(channel);
+
+      // Loop through the 4096 samples
+      for (var sample = 0; sample < inputBuffer.length; sample++) {
+        // make output equal to the same as the input
+        outputData[sample] = fcn(inputData[sample]);
+      }
+    }
+  }
+
+  scriptNode.connect(next);
+  MUSIC.effectsPipeExtend(this, audio, scriptNode);
+
+  this.output = function() {
+    return scriptNode;
+  };
+};
+
+MUSIC.Effects.Attenuator = function(audio, next, factor) {
+  MUSIC.Effects.Formula.bind(this)(audio, next, function(input) {
+    return input * factor();
+  });
+};
+
