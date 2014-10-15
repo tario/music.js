8d5905a550b9b52ab896c16f528e1583068a7153
Dario Seminara <darios3@gmail.com>
2014-10-15 18:33:35 -0300

    implemented syntax highlighting using codemirror library

--- site/controllers.js
+++ site/controllers.js
@@ -1,7 +1,14 @@
-var musicShowCaseApp = angular.module("MusicShowCaseApp", []);
+var musicShowCaseApp = angular.module("MusicShowCaseApp", ['ui.codemirror']);
 
 musicShowCaseApp.controller("MainController", function($scope, $http) {
 	var music = new MUSIC.Context();
+ 	
+ 	$scope.editorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        mode: 'javascript'
+    };
+
 	$http.get("/defaultCode.js").then(function(r) {
 		$scope.code = r.data;
 		eval($scope.code);
