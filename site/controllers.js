3beab4baee6ac0f656e43e90db7c42fe275387ad
Dario Seminara <darios3@gmail.com>
2014-10-15 16:20:32 -0300

    implementing elemental editor using angular

---
+++ site/controllers.js
@@ -0,0 +1,14 @@
+var musicShowCaseApp = angular.module("MusicShowCaseApp", []);
+
+musicShowCaseApp.controller("MainController", function($scope, $http) {
+	$http.get("/defaultCode.js").then(function(r) {
+		$scope.code = r.data;
+		eval($scope.code);
+	});
+
+	$scope.run = function() {
+		eval($scope.code);
+	};
+});
+
+
