5e418a57911b95c124b5027c0e425d6a24df8b77
Dario Seminara <darios3@gmail.com>
2014-11-02 12:40:15 -0300

    refactor test to avoid repeating scale naming data

--- spec/MusicUtilsSpec.js
+++ spec/MusicUtilsSpec.js
@@ -1,13 +1,16 @@
 describe("Music.Utils", function() {
 
   describe("Scale", function() {
-    var noteNumToSemitone = function(scaleName, scaleNum, semitone1, noteNum, semitoneResult) {
-      it(scaleName + " scale should get semitone " + semitoneResult + " from adding " + noteNum + " notes to semitone " + semitone1 , function() {
-        var scale = MUSIC.Utils.Scale(scaleNum);
-        expect(scale.add(semitone1, noteNum)).toBe(semitoneResult);
-      });
+    var scaleTest = function(scaleName, scaleNum) {
+      return function(semitone1, noteNum, semitoneResult) {
+        it(scaleName + " scale should get semitone " + semitoneResult + " from adding " + noteNum + " notes to semitone " + semitone1 , function() {
+          var scale = MUSIC.Utils.Scale(scaleNum);
+          expect(scale.add(semitone1, noteNum)).toBe(semitoneResult);
+        });
+      };
     };
 
-    noteNumToSemitone("C#", 0, 0, 0, 0);
+    var s = scaleTest("C major", 0);
+    s(0, 0, 0);
   });
 });
