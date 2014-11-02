103f88483b3305ebbac432280de763674a3456ce
Dario Seminara <darios3@gmail.com>
2014-11-02 12:54:22 -0300

    made 56 failing test pass (note scale aritmethic

--- src/music/utils.js
+++ src/music/utils.js
@@ -3,11 +3,15 @@ MUSIC.Utils = MUSIC.Utils || {};
 MUSIC.Utils.Scale = function(base) {
   return {
     add: function(notenum, notes) {
-      var ret = notenum + notes * 2;
+      var octaveAdd = Math.floor(notes / 7)
+      var ret;
+
+      notes-= octaveAdd*7;
+      ret = notenum + notes * 2;
       if (notes > 2) {
         ret--;
       }
-      return ret;
+      return ret + octaveAdd*12;
     }
   };
 };
