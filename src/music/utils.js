f5aacd9449623f3451cafa039a62a6dcdedca656
Dario Seminara <darios3@gmail.com>
2014-11-06 16:27:00 -0300

    +8 test pass, addition to notes different from C on C major scale

--- src/music/utils.js
+++ src/music/utils.js
@@ -1,17 +1,30 @@
 (function() {
 MUSIC.Utils = MUSIC.Utils || {};
+var nextNoteHash = {
+  0: 2,
+  2: 4,
+  4: 5,
+  5: 7,
+  7: 9,
+  9: 11,
+  11: 12
+};
+
 MUSIC.Utils.Scale = function(base) {
   return {
     add: function(notenum, notes) {
       var octaveAdd = Math.floor(notes / 7)
+      var noteNumOctave = Math.floor(notenum / 12);
       var ret;
 
       notes-= octaveAdd*7;
-      ret = notenum + notes * 2;
-      if (notes > 2) {
-        ret--;
+
+      ret = notenum - noteNumOctave*12;
+      while (notes > 0) {
+        ret = nextNoteHash[ret];
+        notes--;
       }
-      return ret + octaveAdd*12;
+      return ret + noteNumOctave*12 + octaveAdd*12;
     }
   };
 };
