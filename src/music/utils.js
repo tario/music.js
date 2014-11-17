ddc342bd63f98263471cdb5fd976d3fb3d3f566d
Dario Seminara <darios3@gmail.com>
2014-11-17 00:18:34 -0300

    refactor: rewrite algorithm for scale math add using truefalse hash for fulltoneadd per notenum

--- src/music/utils.js
+++ src/music/utils.js
@@ -1,36 +1,23 @@
 (function() {
 MUSIC.Utils = MUSIC.Utils || {};
 MUSIC.Utils.Scale = function(base) {
-  var nextNoteHash;
+  var toneAdd;
+  var v;
 
-  nextNoteHash = {};
-  var v = [0,2,4,5,7,9,11,12];
-
-  for (var i=0; i<v.length-1; i++) {
-    var value = base+v[i+1];
-    var key = (base+v[i]) % 12;
-    if (value - key > 11) value-=12;
-    nextNoteHash[key] = value;
+  toneAdd = {};
+  v = [0,2,5,7,9];
+  for (var i=0; i<v.length; i++) {
+    toneAdd[(base+v[i]) % 12 ] = true;
   }
 
   return {
     add: function(notenum, notes) {
-      var octaveAdd = Math.floor(notes / 7)
-      var noteNumOctave = Math.floor(notenum / 12);
-      var ret;
-
-      notes-= octaveAdd*7;
-
-      ret = notenum - noteNumOctave*12;
+      var ret = notenum;
       while (notes > 0) {
-        ret = nextNoteHash[ret];
-        if (ret > 11) {
-          ret -= 12;
-          octaveAdd++;
-        }
+        ret+= toneAdd[ret % 12] ? 2 : 1;
         notes--;
       }
-      return ret + noteNumOctave*12 + octaveAdd*12;
+      return ret;
     }
   };
 };
