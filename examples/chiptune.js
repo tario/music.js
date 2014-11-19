537304a77807be04b768a57e88701c53eb6df275
Dario Seminara <darios3@gmail.com>
2014-11-19 16:23:32 -0300

    using scale maths utils on chiptune example

--- examples/chiptune.js
+++ examples/chiptune.js
@@ -1,4 +1,4 @@
-var effects = music;
+var effects = music.gain(0.5).echo({gain: 0.5, delay: 0.1});
 
 var g1 = effects.gain(0.3);
 var g2 = effects.gain(0.3);
@@ -26,23 +26,22 @@ var instrument = new MUSIC.MultiInstrument([
   new MUSIC.Instrument(squareSoundGenerator),
   new MUSIC.Instrument(sineSoundGenerator),
   new MUSIC.Instrument(sawSoundGenerator)
-]).mapNote(function(n){ return n+24;});
+]).mapNote(function(n){ return n+24} /* add one octave to sound */);
 
-var iseq = MUSIC.InstrumentSequence(instrument, 25);
+var iseq = MUSIC.InstrumentSequence(instrument, 50);
+var cmajorscale = MUSIC.Utils.Scale(0);
 arpeggiator = {
   note: function(n) {
     return iseq([
       {num: n, time: 1},
-      {num: n+2, time: 1},
-      {num: n+4, time: 1},
-      {num: n+6, time: 1},
-      {num: n+8, time: 1},
-      {num: n+10, time: 1}
+      {num: cmajorscale.add(n,1), time: 1},
+      {num: cmajorscale.add(n,2), time: 1},
+      {num: cmajorscale.add(n,3), time: 1},
+      {num: cmajorscale.add(n,4), time: 1},
+      {num: cmajorscale.add(n,5), time: 1}
     ]).loop();
   }
 };
 
 instruments.add("Oscillator SIN+SQ+SAW", instrument);
 instruments.add("Arpeggiator", arpeggiator);
-
-
