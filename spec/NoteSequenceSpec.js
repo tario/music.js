d509d4a91c8b767c009814181ad8931012adf776
Dario Seminara <darios3@gmail.com>
2014-12-17 11:50:00 -0300

    +2 failing tests: schedule of events on funseq for a single note

--- spec/NoteSequenceSpec.js
+++ spec/NoteSequenceSpec.js
@@ -11,6 +11,27 @@ describe("Music.NoteSequence", function() {
   });
 
   it("should allow create NoteSequence for FunctionSequence and instrument", function(){
-    var seq = MUSIC.NoteSequence(fakeFunSeq, fakeInstrument);
+    var seq = new MUSIC.NoteSequence(fakeFunSeq, fakeInstrument);
   })
+
+  describe("when instantiated", function() {
+    var noteseq;
+    beforeEach(function() {
+      noteSeq = new MUSIC.NoteSequence(fakeFunSeq, fakeInstrument);
+    });
+
+    describe("when added a note at 0 with duration 100", function() {
+      beforeEach(function(){
+        noteSeq.push([0,0,100]); // noteNum, startTime, duration
+      });
+
+      it("should output to funseq start event at 0", function(){
+        expect(fakeFunSeq.push.calls.argsFor(0)[0].t).toEqual(0);
+      });
+
+      it("should output to funseq end event at 100", function(){
+        expect(fakeFunSeq.push.calls.argsFor(1)[0].t).toEqual(100);
+      });
+    });
+  });
 });
