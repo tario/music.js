bbe981112b8619df8c272cfe99ed6400bd755e17
Dario Seminara <darios3@gmail.com>
2014-12-17 12:19:17 -0300

    +1 test pass: call play on note returned by instrument on start event

--- src/music/noteSequence.js
+++ src/music/noteSequence.js
@@ -2,7 +2,7 @@
 
 MUSIC.NoteSequence = function(funseq, instrument) {
   this.push = function(array){
-    funseq.push({t:0, f: function(){instrument.note(0)}});
+    funseq.push({t:0, f: function(){instrument.note(0).play()}});
     funseq.push({t:100, f: function(){}});
   };
 };
