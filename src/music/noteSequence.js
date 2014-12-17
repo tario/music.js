6bc3a39e4b77a5a600285ecb55c0136815527ad2
Dario Seminara <darios3@gmail.com>
2014-12-17 13:27:04 -0300

    +7 test pass: chanigng notenum on noteseq

--- src/music/noteSequence.js
+++ src/music/noteSequence.js
@@ -3,8 +3,9 @@
 MUSIC.NoteSequence = function(funseq, instrument) {
   this.push = function(array){
     var playing;
+    var noteNum = array[0];
     funseq.push({t:0, f: function(){
-      playing = instrument.note(0).play()
+      playing = instrument.note(noteNum).play()
     }});
     funseq.push({t:100, f: function(){
       playing.stop();
