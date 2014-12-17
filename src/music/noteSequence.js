f9ee40044a41794b05a8b936cf89074e9ae013a9
Dario Seminara <darios3@gmail.com>
2014-12-17 11:52:09 -0300

    +2 test pass: schedule of events for one single note

--- src/music/noteSequence.js
+++ src/music/noteSequence.js
@@ -1,6 +1,10 @@
 (function() {
 
-MUSIC.NoteSequence = function() {
+MUSIC.NoteSequence = function(funseq, instrument) {
+  this.push = function(array){
+    funseq.push({t:0});
+    funseq.push({t:100});
+  };
 };
 
 
