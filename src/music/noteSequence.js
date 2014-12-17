7909a9e3d67aa06119a2b8cbfe2a3b103692182f
Dario Seminara <darios3@gmail.com>
2014-12-17 16:37:42 -0300

    +32 test pass: end event scheduled according note duration

--- src/music/noteSequence.js
+++ src/music/noteSequence.js
@@ -4,10 +4,11 @@ MUSIC.NoteSequence = function(funseq, instrument) {
   this.push = function(array){
     var playing;
     var noteNum = array[0];
+    var duration = array[2]
     funseq.push({t:0, f: function(){
       playing = instrument.note(noteNum).play()
     }});
-    funseq.push({t:100, f: function(){
+    funseq.push({t:duration, f: function(){
       playing.stop();
     }});
   };
