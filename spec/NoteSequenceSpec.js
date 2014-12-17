f88523e5eb8f6d4ef2f05180ccf2b220b83631db
Dario Seminara <darios3@gmail.com>
2014-12-17 11:40:04 -0300

    +1 failing test: instantiation of noteseq

--- spec/NoteSequenceSpec.js
+++ spec/NoteSequenceSpec.js
@@ -1,2 +1,16 @@
 describe("Music.NoteSequence", function() {
+  var fakeFunSeq;
+  var fakeInstrument;
+  beforeEach(function() {
+    fakeFunSeq = {};
+    fakeFunSeq.start = jasmine.createSpy("FunSeq.start");
+    fakeFunSeq.push = jasmine.createSpy("FunSeq.start");
+
+    fakeInstrument = {};
+    fakeInstrument = jasmine.createSpy("instrument.note");
+  });
+
+  it("should allow create NoteSequence for FunctionSequence and instrument", function(){
+    var seq = MUSIC.NoteSequence(fakeFunSeq, fakeInstrument);
+  })
 });
