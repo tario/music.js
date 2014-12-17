bc2ccf6437151c8717e933bc2d9814ca64270161
Dario Seminara <darios3@gmail.com>
2014-12-17 16:46:01 -0300

    +560 test pass: different starting times for notes

--- src/music/noteSequence.js
+++ src/music/noteSequence.js
@@ -4,11 +4,12 @@ MUSIC.NoteSequence = function(funseq, instrument) {
   this.push = function(array){
     var playing;
     var noteNum = array[0];
-    var duration = array[2]
-    funseq.push({t:0, f: function(){
+    var startTime = array[1];
+    var duration = array[2];
+    funseq.push({t:startTime, f: function(){
       playing = instrument.note(noteNum).play()
     }});
-    funseq.push({t:duration, f: function(){
+    funseq.push({t:startTime + duration, f: function(){
       playing.stop();
     }});
   };
