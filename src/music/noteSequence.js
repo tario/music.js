03b5c2f44995644989654f83689a620fea86b364
Dario Seminara <darios3@gmail.com>
2014-12-20 12:57:29 -0300

    refactor: moved method-functions of noteSequence to prototype

--- src/music/noteSequence.js
+++ src/music/noteSequence.js
@@ -1,29 +1,31 @@
 (function() {
 
 MUSIC.NoteSequence = function(funseq) {
-  this.push = function(array){
-    var playing;
-    var noteNum = array[0];
-    var startTime = array[1];
-    var duration = array[2];
-    funseq.push({t:startTime, f: function(ctx){
-      playing = ctx.instrument.note(noteNum).play()
-    }});
-    funseq.push({t:startTime + duration, f: function(){
-      playing.stop();
-    }});
-  };
+  this._funseq = funseq;
+};
 
-  this.makePlayable = function() {
-    return {
-      play: function() {
-        return {
-          stop: function() {
+MUSIC.NoteSequence.prototype.push = function(array){
+  var playing;
+  var noteNum = array[0];
+  var startTime = array[1];
+  var duration = array[2];
+  this._funseq.push({t:startTime, f: function(ctx){
+    playing = ctx.instrument.note(noteNum).play()
+  }});
+  this._funseq.push({t:startTime + duration, f: function(){
+    playing.stop();
+  }});
+};
 
-          }
-        };
-      }
-    };
+MUSIC.NoteSequence.prototype.makePlayable = function() {
+  return {
+    play: function() {
+      return {
+        stop: function() {
+
+        }
+      };
+    }
   };
 };
 
