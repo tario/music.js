8a835ceddfc0346c08c6809734754744dfc40f59
Dario Seminara <darios3@gmail.com>
2014-12-20 13:02:10 -0300

    refactor: moved function-method to prototype for playing and playable of noteseq

--- src/music/noteSequence.js
+++ src/music/noteSequence.js
@@ -4,6 +4,19 @@ MUSIC.NoteSequence = function(funseq) {
   this._funseq = funseq;
 };
 
+MUSIC.NoteSequence.Playable = function(noteseq) {
+  this._noteseq = noteseq;
+};
+MUSIC.NoteSequence.Playable.prototype.play = function() {
+  return new MUSIC.NoteSequence.Playing(this._noteseq);
+};
+
+MUSIC.NoteSequence.Playing = function(noteseq) {
+  this._noteseq = noteseq;
+};
+MUSIC.NoteSequence.Playing.prototype.stop = function() {
+};
+
 MUSIC.NoteSequence.prototype.push = function(array){
   var playing;
   var noteNum = array[0];
@@ -18,15 +31,7 @@ MUSIC.NoteSequence.prototype.push = function(array){
 };
 
 MUSIC.NoteSequence.prototype.makePlayable = function() {
-  return {
-    play: function() {
-      return {
-        stop: function() {
-
-        }
-      };
-    }
-  };
+  return new MUSIC.NoteSequence.Playable(this);
 };
 
 MUSIC.NoteSequence.context = function(instrument) {
