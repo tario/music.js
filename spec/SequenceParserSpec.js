aef7278614eb86fd2ded933a3f7f96c351c0efb0
Dario Seminara <darios3@gmail.com>
2014-12-23 15:25:03 -0300

    +21 failing tests: single notes with alterations (sharp and bemol)

--- spec/SequenceParserSpec.js
+++ spec/SequenceParserSpec.js
@@ -1,13 +1,27 @@
 describe("Music.SequenceParser", function() {
 
   var notes = {
+    "Cb": -1,
     "C": 0,
+    "C#": 1,
+    "Db": 1,
     "D": 2,
+    "D#": 3,
+    "Eb": 3,
     "E": 4,
+    "E#": 5,
+    "Fb": 4,
     "F": 5,
+    "F#": 6,
+    "Gb": 6,
     "G": 7,
+    "G#": 8,
+    "Ab": 8,
     "A": 9,
-    "B": 11
+    "A#": 9,
+    "Bb": 10,
+    "B": 11,
+    "B#": 12
   };
 
   var noteSeq;
