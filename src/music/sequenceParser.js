c89d1c90cbfe64858339b3a42f964164dcbf6ce8
Dario Seminara <darios3@gmail.com>
2014-12-23 17:03:18 -0300

    +21 test pass: notes altered with sharp and bemol

--- src/music/sequenceParser.js
+++ src/music/sequenceParser.js
@@ -1,13 +1,27 @@
 (function() {
 MUSIC.SequenceParser = {};
 var notes = {
+  "Cb": -1,
   "C": 0,
+  "C#": 1,
+  "Db": 1,
   "D": 2,
+  "D#": 3,
+  "Eb": 3,
   "E": 4,
+  "E#": 5,
+  "Fb": 4,
   "F": 5,
+  "F#": 6,
+  "Gb": 6,
   "G": 7,
+  "G#": 8,
+  "Ab": 8,
   "A": 9,
-  "B": 11
+  "A#": 9,
+  "Bb": 10,
+  "B": 11,
+  "B#": 12
 };
 
 MUSIC.SequenceParser.parse = function(input, noteSeq) {
@@ -17,10 +31,10 @@ MUSIC.SequenceParser.parse = function(input, noteSeq) {
 
   firstNote = input[0];
   for (var i=0; i<input.length; i++) {
-    currentCharacter = input[i];
+    currentCharacter = input.slice(i);
     currentNote = notes[currentCharacter];
     if (currentNote!==undefined) {
-      noteSeq.push([currentNote,i,1]);
+      noteSeq.push([currentNote,i,currentCharacter.length]);
     }
   }
 };
