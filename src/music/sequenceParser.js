1b550c045b77faa49f310a4eded6f8f950980ab6
Dario Seminara <darios3@gmail.com>
2014-12-23 11:13:15 -0300

    +7 test pass: parsing of all notes

--- src/music/sequenceParser.js
+++ src/music/sequenceParser.js
@@ -1,7 +1,17 @@
 (function() {
 MUSIC.SequenceParser = {};
+var notes = {
+  "C": 0,
+  "D": 2,
+  "E": 4,
+  "F": 5,
+  "G": 7,
+  "A": 9,
+  "B": 11
+};
+
 MUSIC.SequenceParser.parse = function(input, noteSeq) {
-  noteSeq.push(0,0,1);
+  noteSeq.push(notes[input],0,1);
 };
 
 })();
