d5b30dedd5b7fcd5aed6eb8af8580f38c7b89ef2
Dario Seminara <darios3@gmail.com>
2014-12-23 11:09:50 -0300

    +21 failing tests: parsing of single note

--- spec/SequenceParserSpec.js
+++ spec/SequenceParserSpec.js
@@ -1,3 +1,39 @@
 describe("Music.SequenceParser", function() {
 
+  var notes = {
+    "C": 0,
+    "D": 2,
+    "E": 4,
+    "F": 5,
+    "G": 7,
+    "A": 9,
+    "B": 11
+  };
+
+  var noteSeq;
+  beforeEach(function() {
+    noteSeq = {
+      push: jasmine.createSpy("noteSeq.push")
+    };
+  });
+
+  for (var note in notes) {
+    describe("when parsed note '" + note + "'", function() {
+      beforeEach(function(){
+        MUSIC.SequenceParser.parse(note, noteSeq);
+      });
+
+      it("should call push on noteSeq", function(){
+        expect(noteSeq.push).toHaveBeenCalled();
+      });
+
+      it("should call push on noteSeq with note on semitone " + notes[note], function(){
+        expect(noteSeq.push).toHaveBeenCalledWith(notes[note],0,1);
+      });
+
+      it("should call push on noteSeq ONLY one time", function(){
+        expect(noteSeq.push.calls.count()).toEqual(1);
+      });
+    });
+  };
 });
