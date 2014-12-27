53027bb7afc2562434bfbd0f16254848c719f800
Dario Seminara <darios3@gmail.com>
2014-12-27 18:57:06 -0300

    completed sample music of sasa lvl1

--- examples/astroRoboSasa.js
+++ examples/astroRoboSasa.js
@@ -1,7 +1,9 @@
 // create the sound generator
-var soundGenerator = {
+var squareWave = function(volume){
+  var baseEffect = music.gain(volume);
+  var soundGenerator = {
   freq: function(fr) {
-    return music
+    return baseEffect
       .stopCurve({
         node: function(node) {
           return node.oscillator({type: 'square', frequency: fr});
@@ -10,16 +12,35 @@ var soundGenerator = {
       });
 
   }
+  };
+  
+  return new MUSIC.Instrument(soundGenerator);
 };
 
 // create the instrument from sound generator
-var instrument = 
-    new MUSIC.Instrument(soundGenerator);
+var instrument = squareWave(1.0).mapNote(function(n){return n-12;});
+var bass = squareWave(0.7).mapNote(function(n){return n-12;});
 
-var melody1 = MUSIC.Pattern([[
-    "C4D4E4C4D4C4====....A#4=F4==....", instrument
-  ]],
-  {pulseTime: 100})
 
-instruments.add("Square wave oscillator", instrument.mapNote(function(n){return n+36;}));
-playables.add("Sasa lvl 1 song", melody1);
+var melody1 = MUSIC.Pattern([
+    ["C5==D5==E5==C5==|D5==C5==========|........A#5=====|F5======........", instrument],
+    ["C1==....C1==C2==|....A#1=....D1==|C1==....B#0=Bb1=|....G1==....D1==", bass]
+  ],
+  {pulseTime: 50});
+
+var melody2 = MUSIC.Pattern([
+    ["C5==C5==C5==F5==|D5==D5==D5==G5==|====............|G5======G4====..", instrument],
+    ["................|................|....D1==C1==B0==|A#0=B0==C1==D1==", bass]
+  ],
+  {pulseTime: 50});
+
+window.melody = melody2;
+
+var song = new MUSIC.Song([
+    "XXXYXXXYXXXYXXXYXXXYXXXYXXXYXXXYXXXYXXXYXXXY" 
+  ], {X: melody1, Y: melody2}, {measure: 50*16*4 });
+
+instruments.add("Square wave oscillator", instrument.mapNote(function(n){return n+48;}));
+playables.add("pattern1", melody1);
+playables.add("pattern2", melody2);
+playables.add("Full song", song);
