cc6ceabd0cb551cc903ed30e04593b358e1ee2aa
Dario Seminara <darios3@gmail.com>
2015-01-03 14:12:55 -0300

    restored oscillator.js example to original

--- examples/oscillator.js
+++ examples/oscillator.js
@@ -1,16 +1,12 @@
 // create the sound generator
-var wave = new MUSIC.SoundLib.Wave("src/sound/Kick 1.wav",1000);
-var soundGenerator = {
-  freq: function(fr) {
-            return music
-              .oscillator({wave: wave, frequency: fr});
-  }
-};
+var soundGenerator = 
+            music
+              .oscillator({type: 'square'});
 
 // create the instrument from sound generator
 var instrument = 
     new MUSIC.Instrument(soundGenerator)
-                .mapNote(function(n) { return n - 36*2; });
+                .mapNote(function(n) { return n + 36; });
 
 // add instrument to show on UI
-instruments.add("Custom wave oscillator from file", instrument);
+instruments.add("Square wave oscillator", instrument);
