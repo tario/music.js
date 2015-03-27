4e82d3e0594d61cf585a82f3b7b406130e86ea8f
Dario Seminara <darios3@gmail.com>
2015-03-27 20:00:42 -0300

    fixed stop for song

--- src/music/song.js
+++ src/music/song.js
@@ -1,11 +1,16 @@
 (function() {
 
 var PlayingSong = function(funseq) {
-  this._funseq = funseq;
+  this._context = {playing: []};
+  this._funseqHandler = funseq.start(this._context);
 };
 
 PlayingSong.prototype.stop = function() {
-  this._funseq.stop();
+  this._context.playing.forEach(function(playing) {
+    playing.stop();
+  });
+
+  this._funseqHandler.stop();
 };
 
 var fromPatterns = function(patterns) {
@@ -20,7 +25,7 @@ MUSIC.Song = function(input, patterns, options){
   var measure = options.measure || 500;
   var funseq;
   if (!funseq){
-    clock = MUSIC.Utils.Clock(
+    var clock = MUSIC.Utils.Clock(
       window.performance.now.bind(window.performance),
       setInterval,
       clearInterval,
@@ -44,12 +49,14 @@ MUSIC.Song = function(input, patterns, options){
       var playableArray = patternArray.map(getFromPatterns) 
       var multiPlayable = new MUSIC.MultiPlayable(playableArray);
       var playing;
-      funseq.push({t: j*measure, f: function() {
+      funseq.push({t: j*measure, f: function(context) {
         playing = multiPlayable.play();
+        context.playing.push(playing);
       }});
 
-      funseq.push({t: (j+1)*measure, f: function() {
+      funseq.push({t: (j+1)*measure, f: function(context) {
         playing.stop();
+        context.playing = context.playing.filter(function(x){ return x != playing; });
       }});
 
     })();
@@ -61,7 +68,6 @@ MUSIC.Song.prototype.duration = function() {
 };
 
 MUSIC.Song.prototype.play = function() {
-  this._funseq.start();
   return new PlayingSong(this._funseq);
 };
 
