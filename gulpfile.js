d596eeded272dba647b01c0df9db1581be08695b
Dario Seminara <darios3@gmail.com>
2016-02-11 19:29:05 -0300

    implemented gulp (music.js concat+minify+sourcemaps)

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var rename = require('gulp-rename');
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var DEST = "dist/"
+gulp.task("default", ["build-lib", "build-site"]);
+gulp.task('build-lib', function(cb) {
+  return gulp.src(["src/typecast.js", "src/music.js", "src/music/**/*.js"])
+            .pipe(sourcemaps.init())
+              .pipe(concat("music.min.js"))
+              .pipe(uglify())
+            .pipe(sourcemaps.write("."))
+            .pipe(gulp.dest(DEST));
+});
+
+gulp.task('build-site', function(cb) {
+  console.log("TODO: build-site");
+});
