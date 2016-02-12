d29afade33deedbd20fbc4aaf8dd3dcc0a3ccba5
Dario Seminara <darios3@gmail.com>
2016-02-12 20:23:18 -0300

    Implemented gulp-connect development server

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,35 @@ var gulp = require("gulp");
 var rename = require('gulp-rename');
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
 var sourcemaps = require('gulp-sourcemaps');
 
 var DEST = "dist/"
-gulp.task("default", ["build-lib", "build-site"]);
+gulp.task("build", ["build-lib", "build-site"]);
+gulp.task("build-dev", ["build-lib-dev", "build-site-dev"]);
+gulp.task("default", ["webserver", "watch"]);
+
+gulp.task('webserver', function() {
+  connect.server({livereload: true});
+});
+
+gulp.task('watch', function() {
+    gulp.watch('site/**/*.js', ['build-site-dev']);
+    gulp.watch('src/**/*.js', ['build-lib-dev']);
+})
+
+gulp.task('build-lib-dev', function(cb) {
+  return gulp.src(["src/lib/*.js", "src/typecast.js", "src/music.js", "src/music/**/*.js"])
+            .pipe(concat("music.js"))
+            .pipe(gulp.dest(DEST));
+});
+
+gulp.task('build-site-dev', function(cb) {
+  return gulp.src(["site/lib/*.js", "site/lib/*/*.js", "site/app.js", "site/*.js"])
+            .pipe(concat("site.js"))
+            .pipe(gulp.dest(DEST));
+});
+
 gulp.task('build-lib', function(cb) {
   return gulp.src(["src/lib/*.js", "src/typecast.js", "src/music.js", "src/music/**/*.js"])
             .pipe(sourcemaps.init())
