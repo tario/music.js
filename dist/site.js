ddcdb97886a0a52d6f6870fa5a27436cc8ed85f3
Dario Seminara <darios3@gmail.com>
2016-06-13 22:37:05 -0300

    implemented resulting waveform graph for custom osc

--- dist/site.js
+++ dist/site.js
@@ -12651,6 +12651,10 @@ musicShowCaseApp.directive("musicObjectEditor", ["$timeout", "$http", "TypeServi
 
       scope.parameters = [];
 
+      scope.termschanged = function() {
+        scope.$broadcast('termschanged');
+      };
+
       scope.oscTermsUpdateFromWaveForm = fn.debounce(function(waveform, terms) {
         var waveform = eval("(function(t) { return " + waveform + "; })");
         var count = 512;
@@ -12662,8 +12666,12 @@ musicShowCaseApp.directive("musicObjectEditor", ["$timeout", "$http", "TypeServi
         var ft = new DFT(values.length);
         ft.forward(values);
 
-        terms.sin = ft.real;
-        terms.cos = ft.imag;
+        for (var i=0;i<512;i++) {
+          terms.sin[i] = ft.real[i];
+          terms.cos[i] = ft.imag[i];
+        }
+        scope.$broadcast('termschanged');
+
       },400);
 
       scope.oscTermsUpdate = fn.debounce(function(serie, terms) {
@@ -12672,6 +12680,7 @@ musicShowCaseApp.directive("musicObjectEditor", ["$timeout", "$http", "TypeServi
           for (var n=1;n<512;n++) {
             terms[n]=serie(n);
           }
+          scope.$broadcast('termschanged');
         });
       },400);
 
@@ -12978,6 +12987,75 @@ musicShowCaseApp.directive("keyboard", ["$timeout", function($timeout) {
     }
   };
 
+}]);
+
+musicShowCaseApp.directive("customOscGraph", ["$timeout", function($timeout) {
+  return {
+    scope: {
+      terms: "=terms"
+    },
+    template: '<canvas class="wavegraph"></canvas>',
+    link: function(scope, element, attrs) {
+      var f = function(t){
+        var ret = 0;
+        for (var i=1;i<256;i++) {
+          var a = scope.terms.sin[i];
+          var b = scope.terms.cos[i];
+
+          ret = ret + a * Math.sin(t*i);
+          ret = ret + b * Math.cos(t*i);
+        }
+        return ret;
+      };
+
+      var redraw = function() {
+        var canvas = element.children("canvas")[0];
+        var context = canvas.getContext('2d');
+
+        canvas.width = canvas.clientWidth/4;
+        canvas.height = canvas.clientHeight/4;
+
+        var drawLine = function(x0, y0, x1, y1, color) {
+          context.save();
+          context.beginPath();
+          context.moveTo(x0, y0);
+          context.lineTo(x1, y1);
+          context.strokeStyle = color;
+          context.lineWidth = 1;
+          context.stroke();
+          context.restore(); 
+        };
+
+        var drawFunc = function(color) {
+          context.save();
+          context.save();
+          context.translate(0,canvas.height/2);
+          context.scale(canvas.width,canvas.height/2);
+
+          context.moveTo(0, f(0));
+          for (var i=1; i<64; i++) {
+            var t = i/64;
+            context.lineTo(t, f(t*2*Math.PI));
+          }
+          context.restore();
+          context.lineJoin = 'round';
+          context.lineWidth = 1;
+          context.strokeStyle = color;
+          context.stroke();
+          context.restore();
+        };
+
+        drawLine(0, canvas.height/2, canvas.width, canvas.height/2, 'aqua');
+        drawFunc("#FFF");
+
+
+      };
+
+      scope.$on("termschanged", fn.debounce(function() {
+        redraw();
+      },500));
+    }
+  };
 }]);
 
 var musicShowCaseApp = angular.module("MusicShowCaseApp");
