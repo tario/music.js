8c5e801770ac158129dd038410d80f1f32fc97c6
Dario Seminara <darios3@gmail.com>
2017-05-13 15:26:02 -0300

    connected midi inputs to keyboard using navigator.requestMIDIAccess API

---
+++ site/directives/keyboard.js
@@ -0,0 +1,175 @@
+var musicShowCaseApp = angular.module("MusicShowCaseApp");
+musicShowCaseApp.directive("keyboard", ["$timeout", function($timeout) {
+  return {
+    scope: {
+      instrument: '=instrument'
+    },
+    templateUrl: "site/templates/keyboard.html",
+    link: function(scope, element, attrs) {
+      var midiAccess;
+      var onMIDIMessage = function(event) {
+        var command = event.data[0];
+        var value = event.data[1];
+        var velocity = event.data[2];
+
+        var octaveNumber = Math.floor((value-36)/12);
+        if (octaveNumber < 0) return;
+
+        var oct = scope.octaves[octaveNumber];
+        if (!oct) return;
+
+        if (command === 144) {
+          oct.midi[value % 12] = true;
+          oct.update();
+        } else if (command === 128) {
+          oct.midi[value % 12] = false;
+          oct.update();
+        }
+      };
+
+      var onMIDIFailure = function() {
+
+      };
+
+      var onMIDISuccess = function(midi) {
+        var inputs = midi.inputs.values();
+
+        for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+            input.value.onmidimessage = onMIDIMessage;
+        }
+
+        midiAccess = midi;
+      };
+
+      if (navigator.requestMIDIAccess) {
+          navigator.requestMIDIAccess({
+              sysex: false
+          }).then(onMIDISuccess, onMIDIFailure);
+      }
+
+      scope.$on("$destroy", function() {
+        if (midiAccess) {
+          var inputs = midiAccess.inputs.values();
+          
+          for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+              input.value.onmidimessage = null;
+          }
+        }
+      });
+
+      var keyCodeToNote = {
+              90: 'C', 83: 'C#', 88: 'D',  68: 'D#', 67: 'E',
+              86: 'F', 71: 'F#', 66: 'G', 72: 'G#', 78: 'A', 
+              74: 'A#', 77: 'B'};
+
+      var stopAll = function(x) {
+        return x.stopAll();
+      };
+
+      var update = function(octave) {
+        return octave.update();
+      };
+
+      scope.stopAll = function() {
+        scope.octaves.forEach(stopAll);
+      };
+
+      var octave = function(base) {
+        return {
+          mouse: {},
+          key: {},
+          midi: {},
+          note: [],
+          play: function(idx) {
+            if (this.note[idx]) return;
+
+            this.note[idx] = scope.instrument.note(base+idx).play();
+          },
+          stop: function(idx) {
+            if (!this.note[idx])return;
+            this.note[idx].stop();
+            this.note[idx] = undefined;
+          },
+          update: function() {
+            for (var idx=0; idx<12; idx++) {
+              if (this.mouse[idx] || this.key[idx] || this.midi[idx]) {
+                this.play(idx);
+              } else {
+                this.stop(idx);
+              }
+            }
+
+            $timeout(function(){});
+          },
+          stopAll: function() {
+            this.note.forEach(function(note) {
+              if(note && note.stop) note.stop();
+            });
+            this.note = [];
+          }
+        };
+      };
+
+      var mouseOff = function(octave) {
+        octave.mouse = {};
+        octave.update();
+      };
+
+      scope.mouseLeave = function(octave, idx) {
+        octave.mouse[idx] = false;
+
+        octave.update();
+      };
+
+      scope.mouseEnter = function(octave, idx) {
+        scope.octaves.forEach(mouseOff);
+        octave.mouse[idx] = true;
+
+        scope.octaves.forEach(update);
+      };
+
+      var keyDownHandler = function(e) {
+        if (document.activeElement.tagName.toLowerCase() === "input") return;
+
+        var keyCode = e.keyCode;
+        var noteName = keyCodeToNote[keyCode];
+        if (!noteName) return;
+
+        var idx = MUSIC.noteToNoteNum(noteName);
+        scope.octaves[1].key[idx] = true;
+        scope.octaves[1].update();
+
+        scope.$digest();
+      }
+
+      var keyUpHandler = function(e) {
+        var keyCode = e.keyCode;
+        var noteName = keyCodeToNote[keyCode];
+        if (!noteName) return;
+
+        var idx = MUSIC.noteToNoteNum(noteName);
+        scope.octaves[1].key[idx] = false;
+        scope.octaves[1].update();
+
+        scope.$digest();
+      }
+
+      $(document).bind("keydown", keyDownHandler);
+      $(document).bind("keyup", keyUpHandler);
+
+      scope.$on("$destroy", function() {
+        $(document).unbind("keydown", keyDownHandler);
+        $(document).unbind("keyup", keyUpHandler);
+        scope.octaves.forEach(function(octave) {
+          octave.stopAll();
+        });
+      });
+
+      scope.octaves = [24,36,48,60,72].map(octave);
+      scope.$watch("instrument", function(instrument) {
+        scope.instrument = instrument;
+      });
+    }
+  };
+
+}]);
